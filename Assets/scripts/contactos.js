798f5358c76dd05bd2f2d18ae42afd4ca38dbe52
darriola2021670 <darriola2021670@kinal.edu.gt>
2024-01-31 01:07:30 -0600

    se arreglaron errores en los detalle contacto en el js

--- Assets/scripts/contactos.js
+++ Assets/scripts/contactos.js
@@ -1,30 +1,23 @@
-
-function redirectToDetalle() {
-  alert("Redirigiendo a la página de detalle");
-  window.location.href = "detalleContacto1.html";
-}
-
-function redirectToDetalle() {
-  alert("Redirigiendo a la página de detalle");
-  window.location.href = "detalleContacto2.html";
-}
-
-function redirectToDetalle() {
-  alert("Redirigiendo a la página de detalle");
-  window.location.href = "detalleContacto3.html";
-}
-
-function redirectToDetalle() {
-  alert("Redirigiendo a la página de detalle");
-  window.location.href = "detalleContacto4.html";
-}
-
-function redirectToDetalle() {
-  alert("Redirigiendo a la página de detalle");
-  window.location.href = "detalleContacto5.html";
+function redirectToDetalle(contacto) {
+  if (contacto === 'DiegoRene') {
+    window.location.href = 'detalleContacto1.html';
+  } else if (contacto === 'RosaDiaz') {
+    window.location.href = 'detalleContacto2.html';
+  } else if (contacto === 'JoseRoberto'){
+    window.location.href = 'detalleContacto3.html';
+  } else if (contacto === 'PabloAndre'){
+    window.location.href = 'detalleContacto4.html';
+  } else if (contacto === 'CesarAndre'){
+    window.location.href = 'detalleContacto5.html';
+  }
 }
 
 function redirectToAgregarContacto() {
   alert("Redirigiendo a la página de agregar contacto");
   window.location.href   = "nuevoContacto.html";
+}
+
+function redirectToFavoritos() {
+  alert("Redirigiendo a la página de contactos favoritos");
+  window.location.href   = "favoritos.html";
 }
