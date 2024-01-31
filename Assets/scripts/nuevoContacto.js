dc37f522ebc93b751177b14dda4bf476e42d77fd
darriola2021670 <darriola2021670@kinal.edu.gt>
2024-01-31 00:13:18 -0600

    se llenaron los campos del html, css y js ya esta funcional

--- Assets/scripts/nuevoContacto.js
+++ Assets/scripts/nuevoContacto.js
@@ -1,17 +1,20 @@
 function guardarRegistro() {
     var nombre = document.getElementById('nombre').value;
-    var edad = document.getElementById('edad').value;
+    var numero = document.getElementById('numero').value;
+    var correo = document.getElementById('correo').value;
 
-    if (nombre.trim() === '' || edad.trim() === '') {
+    if (nombre.trim() === '' || numero.trim() === '' || correo.trim() === '') {
         alert('Por favor, completa todos los campos.');
         return;
     }
 
     var lista = document.getElementById('registrosList');
     var nuevoRegistro = document.createElement('li');
-    nuevoRegistro.textContent = 'Nombre: ' + nombre + ', Edad: ' + edad;
+    nuevoRegistro.textContent = 'Nombre: ' + nombre + ', Número: ' + numero + ', Correo: ' + correo;
     lista.appendChild(nuevoRegistro);
 
     document.getElementById('nombre').value = '';
-    document.getElementById('edad').value = '';
+    document.getElementById('numero').value = '';
+    document.getElementById('correo').value = '';
 }
+
