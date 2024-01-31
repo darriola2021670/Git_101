848225f867882d50596ff47591855cb97fe2d1a5
darriola2021670 <darriola2021670@kinal.edu.gt>
2024-01-30 21:41:13 -0600

    se agregaron los links de detalle de contacto

--- Assets/scripts/contactos.js
+++ Assets/scripts/contactos.js
@@ -1,7 +1,27 @@
 
 function redirectToDetalle() {
   alert("Redirigiendo a la página de detalle");
-  window.location.href  = "detalle.html";
+  window.location.href = "detalleContacto1.html";
+}
+
+function redirectToDetalle() {
+  alert("Redirigiendo a la página de detalle");
+  window.location.href = "detalleContacto2.html";
+}
+
+function redirectToDetalle() {
+  alert("Redirigiendo a la página de detalle");
+  window.location.href = "detalleContacto3.html";
+}
+
+function redirectToDetalle() {
+  alert("Redirigiendo a la página de detalle");
+  window.location.href = "detalleContacto4.html";
+}
+
+function redirectToDetalle() {
+  alert("Redirigiendo a la página de detalle");
+  window.location.href = "detalleContacto5.html";
 }
 
 function redirectToAgregarContacto() {
